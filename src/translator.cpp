5732f7066e196b5d7fa792aa07b5cd8a85dd07c3
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-01-17 12:05:22 +0100

    Exercice1

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <string>
+#include <set>
 
-int main()
-{
+std::set<std::string> make_exit_commands(){
+        return { "e", "exit", "q", "quit"};
+}
+
+int main(){
+
+    std::string chaine;
+    auto exitList = make_exit_commands();
     while (true)
     {
         std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
+        std::cin >> chaine;
+        //if(chaine == "q" || chaine == "quit" || chaine == "e" || chaine == "exit"){
+        if(exitList.find(chaine) != exitList.end()){
+            break;
+        }
     }
 
     return 0;
