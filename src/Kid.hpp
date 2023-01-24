24e661566f25454a901e7bad2ca2a57c96390565
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-01-24 14:34:54 +0100

    Test 12

--- src/Kid.hpp
+++ src/Kid.hpp
@@ -1,1 +1,30 @@
 #pragma once
+#include <string>
+#include <ostream>
+
+class Kid{
+
+
+    public:
+        Kid (const std::string& name, int money)
+        : _money {money},
+        _name {name}
+        {}
+    
+        const std::string& get_name() const{
+            return _name;
+        }
+
+        int get_money() const{
+            return _money;
+        }
+    
+    private :
+        int _money = 0;
+        std::string _name;
+
+};
+
+std::ostream& operator<<(std::ostream& stream, const Kid& kid){
+    return stream << kid.get_name() << " has " << kid.get_money() << " euros.";
+}
