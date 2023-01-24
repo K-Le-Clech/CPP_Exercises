d775e9571a70127ac533ac56bcd9d1adbc9a4e01
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-01-24 11:44:50 +0100

    test 1 à 5

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
 class Plush{
-    
+    public :
+        Plush(){}
+        Plush(int cost)
+            : _cost {cost}
+            {}
+        
+
+        int get_cost() const{
+            return _cost;
+        }
+
+        void set_cost(int new_cost) {
+            _cost = new_cost;
+        }
+
+
+    private :
+        int _cost = 10;
 };
