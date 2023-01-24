ca32470266856f79d1d9271d066218bb7c6a5329
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-01-24 11:52:39 +0100

    Test 7

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -11,6 +11,16 @@ class PlushStore{
             return _name;
         }
 
+        unsigned int get_wealth_amount() const {
+            return _amount;
+        }
+
+        unsigned int get_stock_size() const {
+            return _stock;
+        }
+
     private:
-        std::string _name; 
+        std::string _name;
+        unsigned int _amount = 0u;
+        unsigned int _stock = 0u;
 };
