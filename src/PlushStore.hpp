b1aed9debe30bc218b5c5ca086b011a2f13f6113
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-01-24 11:45:00 +0100

    test 6

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#include <string>
+
+class PlushStore{
+
+    public:
+        PlushStore (const std::string& name)
+        : _name {name}
+        {}
+        const std::string& get_name() const {
+            return _name;
+        }
+
+    private:
+        std::string _name; 
+};
