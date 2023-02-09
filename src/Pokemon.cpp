408cec47b31d1252642e6027be3a268fc8a775b5
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-02-09 15:19:04 +0100

    Test1 passed

--- src/Pokemon.cpp
+++ src/Pokemon.cpp
@@ -1,1 +1,29 @@
 #include "Pokemon.h"
+
+Pokemon::Pokemon(const std::string& name)
+    : _name {name},
+    _id { _maxId++}
+    {}
+
+Pokemon::Pokemon(const Pokemon& other)
+    : _name { other._name},
+    _id { _maxId++}
+    {}
+
+Pokemon& Pokemon::operator=(const Pokemon& other){
+    if(this!=&other){
+        _name = other._name;
+
+    }
+    return *this;
+}
+
+const std::string& Pokemon::name()const {
+    return Pokemon::_name;
+}
+
+int Pokemon::id() const {
+    return Pokemon::_id;
+}
+
+int Pokemon::_maxId = 0;
--- src/Pokemon.h
+++ src/Pokemon.h
@@ -1,7 +1,21 @@
 #pragma once
 
 #include <string>
+#include <string>
 
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{
+    public :
+        Pokemon (const std::string& name);
+        Pokemon(const Pokemon& other);
+        Pokemon& operator=(const Pokemon& other);
+        const std::string& name() const;
+        int id() const;
+
+    private:
+        std::string _name;
+        static int _maxId;
+        int _id;
+};
+
