7c5a1afe3430c27a1c80e8bdfd11554154e13272
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-02-14 09:01:15 +0100

    Test 1 passed

--- src/Card.hpp
+++ src/Card.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include <string>
+#include "Monster.hpp"
+#include "Spell.hpp"
+#include "Trap.hpp"
+
+enum class CardType{
+    Monster,
+    Spell,
+    Trap,
+};
+
+std::string to_string(CardType elem){
+    switch(elem){
+        case CardType::Monster : return "Monster";
+        case CardType::Spell : return "Spell";
+        case CardType::Trap : return "Trap";
+        default : return "";
+    }
+}
