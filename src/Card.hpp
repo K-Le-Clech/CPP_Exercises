15d054fbe783a19823a36e55cccf6bfe20a4e0f5
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-02-14 09:14:32 +0100

    test 2 passed

--- src/Card.hpp
+++ src/Card.hpp
@@ -18,3 +18,42 @@ std::string to_string(CardType elem){
         default : return "";
     }
 }
+
+class Card{
+
+    public :
+        Card(const std::string& id, CardType type)
+        : _id { id }
+        , _type { type }
+        {}
+
+        void set_name(const std::string& name){
+            _name = name;
+        }
+
+        void set_description(const std::string& description){
+            _description = description;
+        }
+
+        const std::string& get_id() const{
+            return _id;
+        }
+
+        CardType get_type() const{
+            return _type;
+        }
+
+        const std::string& get_name() const{
+            return _name;
+        }
+
+        const std::string& get_description() const{
+            return _description;
+        }
+
+    private :
+        std::string _id = "";
+        CardType _type;
+        std::string _name = "";
+        std::string _description = "";
+};
