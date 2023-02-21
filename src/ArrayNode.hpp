42285bfd21efc29f9269099f9a4698aaa02408dc
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-02-21 12:46:05 +0100

    Start q11

--- src/ArrayNode.hpp
+++ src/ArrayNode.hpp
@@ -2,6 +2,8 @@
 #include "Node.hpp"
 #include "NodeKind.hpp"
 #include <memory>
+#include <vector>
+#include <string>
 
 class ArrayNode : public Node{
 
@@ -14,14 +16,27 @@ class ArrayNode : public Node{
         }
 
         std::string print() const{
-            return "[]";
+            std::string answer = "[";
+            for(auto elem : _array){
+                answer += "," + std::to_string(elem);
+            }
+            return answer + "]";
         }
 
         static std::unique_ptr<ArrayNode> make_ptr(){
             return std::make_unique<ArrayNode>();
         }
 
-        int child_count(){
-            return 0;
+        int child_count() const{
+            return _nb_fils;
         }
+
+        void push_back(std::unique_ptr<ArrayNode> fils){
+            _nb_fils++;
+
+        }
+    
+    private:
+        std::vector<Node> _array = {};
+        int _nb_fils = 0;
 };
